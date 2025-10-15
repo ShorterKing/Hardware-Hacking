7a18ba33edf46bc79d106fb24285b53262d519be
AKSHAT Singh <84120105+ShorterKing@users.noreply.github.com>
2025-10-16 00:04:39 +0530

    Update Commands-To-Know.sh

--- Commands-To-Know.sh
+++ Commands-To-Know.sh
@@ -2,3 +2,6 @@
 
  #Copy all the firmware files simultaneously
 for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13; do dd if=/dev/mtd$i of=mtd$i.bin bs=1M; done
+
+
+for i in $(seq 0 13); do dd if=/dev/mtdblock$i of=mtdblock$i.bin bs=1M; done
